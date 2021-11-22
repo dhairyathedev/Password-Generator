9bc442e4d08a93689cbefd710a95724d0c3985dc
SnowBit <dtshah24022006@gmail.com>
2021-11-22 18:07:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import copyTextToClipboard from 'https://cdn.skypack.dev/copy-text-to-clipboard'
 const elem = document.getElementById('password')
 
 function randomPassword() {
+  let result = '';
     let length = 8
-    let result = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
@@ -14,6 +14,7 @@ function randomPassword() {
     elem.innerText = result;
     copyTextToClipboard(result);
     confetti();
+    passwordStrength(result);
   }
 
 document.body.addEventListener('keyup', (event)=>{
@@ -47,3 +48,32 @@ function toggleLight(){
     document.body.style.color = "#000"
 }
 light.addEventListener('click', toggleLight)
+
+// Password Strength Detection
+function passwordStrength(password) {
+  let strength = 0
+  if (password.length > 8) {
+      strength += 1
+  }
+  if (password.match(/[a-z]/)) {
+      strength += 1
+  }
+  if (password.match(/[A-Z]/)) {
+      strength += 1
+  }
+  if (password.match(/[0-9]/)) {
+      strength += 1
+  }
+  if (password.match(/[^a-zA-Z0-9]/)) {
+      strength += 1
+  }
+    if (strength === 1) {
+      console.log('week');
+  }
+  if (strength === 2) {
+      console.log('GOod');
+  }
+  if (strength === 3) {
+      console.log('strong');
+  }
+}
