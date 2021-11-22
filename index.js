e24fc5fa3ab085b1d231eb83bb732a1813927d15
SnowBit <dtshah24022006@gmail.com>
2021-11-22 17:53:22 +0530

    New layout

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 import copyTextToClipboard from 'https://cdn.skypack.dev/copy-text-to-clipboard';
+
 const elem = document.getElementById('password')
 
 function randomPassword() {
-    let length = document.getElementById('length').value
+    let length = 8
     let result = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    elem.value = result;
-    copyTextToClipboard(result)
+    elem.innerText = result;
+    copyTextToClipboard(result);
+    confetti();
   }
 
 document.body.addEventListener('keyup', (event)=>{
@@ -20,5 +23,27 @@ document.body.addEventListener('keyup', (event)=>{
       }
       
 })
-document.getElementById('generate').addEventListener('click', randomPassword)
+elem.addEventListener('click', randomPassword)
 randomPassword()
+
+
+// Dark Light Toggle
+const dark = document.getElementById('dark')
+const light = document.getElementById('light')
+
+function toggleDark(){
+    dark.classList.add('d-none')
+    light.classList.remove('d-none')
+    document.body.style.backgroundColor = "#212224"
+    document.body.style.color = "#fff"
+}
+
+dark.addEventListener('click', toggleDark)
+
+function toggleLight(){
+    light.classList.add('d-none')
+    dark.classList.remove('d-none')
+    document.body.style.backgroundColor = "#fff"
+    document.body.style.color = "#000"
+}
+light.addEventListener('click', toggleLight)
